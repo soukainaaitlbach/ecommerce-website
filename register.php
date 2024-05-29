07f468b486149b5ebc3f706c781c116bd966acb1
soukainaaitlbach <aitlbachsoukaina@gmail.com>
2024-05-29 19:42:48 +0200

    Update register.php

--- register.php
+++ register.php
@@ -4,17 +4,17 @@ include ('admin/config.php');
 
 if(isset($_POST['submit'])){
 
-   $name = mysqli_real_escape_string($conn, $_POST['name']);
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
-   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+   $name = mysqli_real_escape_string($connect, $_POST['name']);
+   $email = mysqli_real_escape_string($connect, $_POST['email']);
+   $pass = mysqli_real_escape_string($connect, md5($_POST['password']));
+   $cpass = mysqli_real_escape_string($connect, md5($_POST['cpassword']));
 
-   $select = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+   $select = mysqli_query($connect, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
 
    if(mysqli_num_rows($select) > 0){
       $message[] = 'user already exist!';
    }else{
-      mysqli_query($conn, "INSERT INTO `users`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
+      mysqli_query($connect, "INSERT INTO `users`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
       $message[] = 'registered successfully!';
       header('location:login.php');
    }
